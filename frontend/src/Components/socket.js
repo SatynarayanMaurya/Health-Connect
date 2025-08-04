3142f370656284bdd3f850453f777f4516530b10
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2025-08-04 21:54:55 +0530

    Update socket.js

--- frontend/src/Components/socket.js
+++ frontend/src/Components/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // Replace with your backend Socket.IO server URL
-const SOCKET_URL = "http://localhost:4000";
+const SOCKET_URL = "https://health-connect-u164.onrender.com";
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"], // Ensures faster communication
